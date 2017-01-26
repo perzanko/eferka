60dad9218260abde87bde97f8e11afe93469ef0c
Kacper Perzankowski <kacper@devbrothers.pl>
2017-01-26 01:32:47 +0100

    form, icons, fix styles, course template

--- wp-content/themes/devbrothers/page-custom.php
+++ wp-content/themes/devbrothers/page-custom.php
@@ -72,7 +72,7 @@ Template Name: Frontend
               <li class="course__box__description__item">Emisja - jak zwiększyć zasięg odbiorców</li>
             </ul>
             <div class="course__box__meta">
-              <a href="#" class="button button--small">Kup teraz</a>
+              <a href="#" class="button button--small">Zobacz</a>
               <span class="price">349 zł</span>
             </div>
           </div>
@@ -115,6 +115,115 @@ Template Name: Frontend
     <div class="row">
       <div class="col-xs-12">
         <p>Morbi leo risus, porta ac consectetur ac, <a href="#">vestibulum at eros</a>. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor. Donec ullamcorper nulla non metus auctor fringilla. <br>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam porta sem malesuada magna mollis euismod.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<div style="margin-top: 150px">
+</div>
+<section class="bg-white">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-4">
+        <div class="icon">
+          <div class="icon__img">
+            <img src="<?php get_site_URL(); ?>/eferka/wp-content/themes/devbrothers/assets/lokalizacja.png" alt="<?php bloginfo('description'); ?>">
+          </div>
+          <h3 class="icon__title">Lokalizacja</h3>
+          <p class="icon__description">Ucz się w centrum Warszawy.<br>Wszystkie nasze kursy odbywają się w dogodnych lokalizacjach w centrum stolicy</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-4">
+        <div class="icon">
+          <div class="icon__img">
+            <img src="<?php get_site_URL(); ?>/eferka/wp-content/themes/devbrothers/assets/kadra.png" alt="<?php bloginfo('description'); ?>">
+          </div>
+          <h3 class="icon__title">Kadra</h3>
+          <p class="icon__description">Ucz się z najlepszymi.<br>Nasze kursy prowadzą najlepsi specjaliści w dziedzinie techniki audiowizualnej i marketingu.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-4">
+        <div class="icon">
+          <div class="icon__img">
+            <img src="<?php get_site_URL(); ?>/eferka/wp-content/themes/devbrothers/assets/certyfikat.png" alt="<?php bloginfo('description'); ?>">
+          </div>
+          <h3 class="icon__title">Certyfikat</h3>
+          <p class="icon__description">Otrzymaj certyfikat.<br>Po ukończeniu kursu otrzymasz certyfikat potwierdzający nabyte umiejętności</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bg-red">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="form">
+          <h4 class="form__title">Wyślij wiadomość</h4>
+          <form class="form__form" action="index.html" method="post">
+            <label for=""></label><input id="name" type="text" name="name" placeholder="Imię i nazwisko">
+            <label for=""></label><input id="name" type="email" name="email" placeholder="E-mail">
+            <label for=""></label><input id="name" type="text" name="topic" placeholder="Temat">
+            <label for=""></label><textarea id="name" name="textarea" rows="5" cols="80" placeholder="Treść"></textarea>
+            <input class="button" type="submit" name="submit" value="Wyślij">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div style="margin-top: 150px">
+</div>
+<section class="bg-white">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h2 class="heading">Pierwszy milion na Youtubie</h2>
+          <img class="course__box__img course__box__img--full" src="<?php echo get_site_URL(); ?>/wp-content/themes/devbrothers/assets/youtube-course.jpg" alt="<?php bloginfo('description'); ?>">
+          <ul class="course__box__description course__box__description--big clearfix">
+            <li class="course__box__description__item">Pomysł - jak zbudować projekt</li>
+            <li class="course__box__description__item">Inspiracje - jak się pozycjonować</li>
+            <li class="course__box__description__item">Technika - jak zrealizować materiał</li>
+            <li class="course__box__description__item">Emisja - jak zwiększyć zasięg odbiorców</li>
+          </ul>
+          <hr>
+          <p>YouTubowe ABC dla początkujących i średnio zaawansowanych.<br><br>Chcesz nagrać teledysk, zostać nowym Sylwestrem Wardęgą, wypromować produkt lub przemówić do ludzi z ekranu? Wspaniale! Zobacz, co warto zrobić, aby Twój film wyglądał jak produkcja rodem z Hollywood.<br><br>Podczas warsztatów nauczysz się, jak robić jakościowe filmy do internetu. W czasie cyklu spotkań przejdziesz przez wszystkie etapy procesu filmowego:</p>
+          <strong>1. Pomysł</strong>
+          <ul>
+            <li>Co mnie wyróżnia</li>
+            <li>Sprzęt i środki</li>
+            <li>Uwaga widzów</li>
+          </ul>
+          <strong>2. Sprzęt</strong>
+          <ul>
+            <li>Telefon, lustrzanka, kamera, dron</li>
+            <li>Prosty zestaw dźwiękowy</li>
+            <li>Oświetlenie, statywy i inne</li>
+          </ul>
+          <strong>3. Filmowanie</strong>
+          <ul>
+            <li>Rodzaje ujęć</li>
+            <li>Co filmować</li>
+            <li>Opowiadanie obrazem</li>
+          </ul>
+          <strong>4. Obróbka</strong>
+          <ul>
+            <li>Programy montażowe</li>
+            <li>Banki muzyki i ujęć</li>
+            <li>Animacje, napisy, koloryzacja</li>
+          </ul>
+          <strong>5. Emisja</strong>
+          <ul>
+            <li>Formaty i kryptonimy:</li>
+            <li>SD, HD, 4K</li>
+            <li>mpg, mov, avi, wmv</li>
+            <li>PAL, bitrate, fps i inne</li>
+          </ul>
+          <hr>
+          <div class="course__box__meta course__box__meta--left">
+            <span class="price">349 zł</span>
+            <a href="#" class="button button">Zapisz się</a>
+          </div>
       </div>
     </div>
   </div>
